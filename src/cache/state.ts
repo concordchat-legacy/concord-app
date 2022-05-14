0e2b12451cbf489e7ab926e38dbd41ac36ef885f
Vincent <vincentbusiness55@gmail.com>
2022-05-14 11:43:19 +0800

    Rewrite App to `next.js`
    * feat: initial rewrite
    
    * fix: stuff
    
    * Create LICENSE.md
    
    * feat: finishing touches

--- src/cache/state.ts
+++
@@ -1,8 +0,0 @@
-import { makeAutoObservable } from "mobx";
-
-
-export default class State {
-    constructor() {
-        makeAutoObservable(this)
-    }
-}
--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-    return (
-        <div>
-            <h1>Uh oh... It seems like you cought us! We're still building this.</h1>
-        </div>
-    )
-}
-
-export default Home
